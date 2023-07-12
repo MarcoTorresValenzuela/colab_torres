39ace1f22f0ba61582803faee3165bf30412ccb3
Marco Torres <mtorreva@gmail.com>
2023-07-12 23:28:40 +0000

    realizacion

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 
 def v_index(request):
-    context = {}
+    context = {
+        'course1' : Course.objects.get(name = 'Matematica Avanzada'),
+        'course2' : Course.objects.get(name = 'Literatura'),
+    }
     return render(request, 'index.html',context)
 
-def v_course(request):
+def v_course(request, course_id):
     context = {}
     return render(request, 'course.html', context)
