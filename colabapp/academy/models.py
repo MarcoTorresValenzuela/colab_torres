a0d972012291bda65defa6bd5490062f38e578dd
Marco Torres <mtorreva@gmail.com>
2023-07-13 01:52:01 +0000

    boton subscripcion creado

--- colabapp/academy/models.py
+++ colabapp/academy/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Teacher(models.Model):
-    first_name = models.CharField(max_length =128)
-    last_name = models.CharField(max_length =128)
+class Teacher(User):
+    bio = models.CharField(max_length = 500)
     
 class Course(models.Model):
     name = models.CharField(max_length =128)
@@ -14,10 +14,9 @@ class Subject(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete = models.PROTECT)
     start_date = models.DateField()
 
-class Student(models.Model):
-    first_name = models.CharField(max_length =128)
-    last_name = models.CharField(max_length =128)
-    email = models.CharField(max_length =128)
+class Student(User):
+    pass
+
 
 class Subscription(models.Model):
     subject_id = models.ForeignKey(Subject, on_delete = models.PROTECT)
--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Course
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 
@@ -11,5 +12,12 @@ def v_index(request):
     return render(request, 'index.html',context)
 
 def v_course(request, course_id):
-    context = {}
+    context = {
+        'course': Course.objects.get(id = course_id)
+    }
     return render(request, 'course.html', context)
+
+@login_required(login_url = "/admin/login")
+@permission_required('academy.add_subscribe', login_url = "/admin/login")
+def v_subscribe(request, course_id):
+    pass
